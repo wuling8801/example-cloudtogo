c6b00e596574ac4691abfe9091b6288e4d6e4ee0
18833333333 <18833333333@cloudtogo.cn>
2020-02-06 13:31:25 +0000

    Cloud To Go is Amazing!

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "20200205-commit"
+    return "2020-0206-github-commit"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
