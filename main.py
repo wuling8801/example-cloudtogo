23631b4751800ee384899a061ae8e58c1ef6989d
18866666666 <18866666666@cloudtogo.cn>
2020-02-28 11:52:37 +0000

    Cloud To Go is Amazing!

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "testtesttesttest"
+    return "testtesttesttest12121212"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
