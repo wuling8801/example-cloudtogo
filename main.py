f030523b3946b43c6b3bae41b5f8ebaac4fa809f
18866666666 <18866666666@cloudtogo.cn>
2020-02-28 12:11:35 +0000

    Cloud To Go is Amazing!

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 # 请您来修正Hello, word的拼写错误吧！
 # 请在修改完成后，通过主菜单 Git/Commit ... 菜单项完成代码的Commit 和 Push。
 # Push完成后回到Factory ( http://factory.cloudtogo.cn/project/blueprint?id=last )，用同样的方法发布一个新实例即可看到修改后的效果。
-#  测试这个自动触发水电费水电费第三方的额外aaaaaaadfdf  fdsfdsfdsfsaaaaa啊啊啊多福多寿
+#  测试这个自动触发水电费水电费第三方的额外aaaaaaadfdf  fdsfdsfdsfsaaaaa啊啊啊多福多寿双方都
 a
 
 
