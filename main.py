54db7a31c132cba9a7667539ec5c5946cdde5dda
CloudToGoTest <57251311+CloudToGoTest@users.noreply.github.com>
2020-03-02 10:20:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "testtesttesttest133333333333"
+    return "实例数超过5触发自动错误提示"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
