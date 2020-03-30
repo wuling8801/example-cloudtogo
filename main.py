d6ac0ee1136960092bf8019e8a5eefc157b40092
wuling8801 <51203148+wuling8801@users.noreply.github.com>
2020-03-30 21:19:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-#集合间的关系操作
-s1 = {1,2,3,4,5,6}
-s2 = {6,5,4,3,2,1}
-#判断两个集合的元素是否相同
-print(s1 == s2)
+明天有几天  21:18:04
+#!/bin/python
+# -- coding: utf-8 --
 
-s3 = {4,5,6}
-s4 = {4,5,6,7,8,9}
-#issubset是否为子集
-print(s3.issubset(s4))
-#issuperset是否为父集
-print(s4.issuperset(s3))
+__author__ = 'cloudtogo'
 
-s5 = {5}
-s6 = {1,3,5,7,9}
-#isdisjoint函数判断两个集合是否存在重复元素
-#True代表不存在重复，False代表存在重复
-print(s5.isdisjoint(s6))
+def hello():
+
+    body = '您可以通过创建一个袜子电商网站来体验行云趣码一站式开发平台更浩瀚的功能。'
+    print(body)
+
+if __name__ == '__main__':
+    hello()
