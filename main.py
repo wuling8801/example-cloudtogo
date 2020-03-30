af6f9a267eb86eea326357daabf3ffd7007fc8a6
wuling8801 <51203148+wuling8801@users.noreply.github.com>
2020-03-30 21:12:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-num = int(input("请输入一个三位数："))
-gw = num % 10
-print(gw)
-sw = int((num / 10) % 10)
-print(sw)
-bw = int(num / 100)
-print(bw)
-total = gw**3+sw**3+bw**3
-print(total)
-if num == total:
-    print("{0}是水仙花".format(num))
-else:
-    print("{0}不是水仙花".format(num))
+#集合间的关系操作
+s1 = {1,2,3,4,5,6}
+s2 = {6,5,4,3,2,1}
+#判断两个集合的元素是否相同
+print(s1 == s2)
+
+s3 = {4,5,6}
+s4 = {4,5,6,7,8,9}
+#issubset是否为子集
+print(s3.issubset(s4))
+#issuperset是否为父集
+print(s4.issuperset(s3))
+
+s5 = {5}
+s6 = {1,3,5,7,9}
+#isdisjoint函数判断两个集合是否存在重复元素
+#True代表不存在重复，False代表存在重复
+print(s5.isdisjoint(s6))
